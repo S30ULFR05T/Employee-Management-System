82a118804e5fbf21cf9b121d6ff04250153cfde8
Suraj Kumar <140057403+S30ULFR05T@users.noreply.github.com>
2024-03-16 15:55:42 +0530

    Update Question1-Coding.js

--- Question1-Coding.js
+++ Question1-Coding.js
@@ -1,11 +1,11 @@
-const students = [
+const students = [                                       // Providing all given data of Students
     { name: "Dhishan Debnath", Roll: 1 },
     { name: "Animesh Gupta", Roll: 2 },
     { name: "Tapas Sen", Roll: 3 },
     { name: "Misti Dutta", Roll: 4 },
     { name: "Chini Misra", Roll: 5 }
 ];
-const Details = [
+const Details = [                                          // Providing all Details of Student
     { Roll: 5, subjects: { math: 35, english: 56, chemistry: 76, computer: 68 } },
     { Roll: 3, subjects: { math: 33, chemistry: 12, computer: 50, english: 35 } },
     { Roll: 1, subjects: { math: 55, english: 75, chemistry: 76, computer: 94 } },
@@ -14,20 +14,21 @@ const Details = [
 ];
 
 
-function generateStudentMarkSheets(students, Details) {
+function generateStudentMarkSheets(students, Details) {                     // Creating function named as generateStudentMarkSheets()
     const studentsMarkSheets = [];
 
-    students.forEach(student => {
-        const studentDetails = Details.find(detail => detail.Roll === student.Roll);
-        const marks = { ...student, ...studentDetails.subjects };
+    students.forEach(student => {                                          // Checking every data
+        const studentDetails = Details.find(detail => detail.Roll === student.Roll);                  // Finding student details using roll no as primary key
+        const marks = { ...student, ...studentDetails.subjects };                                     // spreading and opening all data of students and subject marks
         const totalMarks = Object.values(marks).reduce((total, mark) => ((typeof mark) === "string")? total+0 :  Number(total + mark), 0); // total act as accumulator and mark act as currentValue (current marks)
-        const status = totalMarks >= 200 ? "pass" : "fail";
-        studentsMarkSheets.push({ ...marks, total: totalMarks, status });
+        const status = totalMarks >= 200 ? "pass" : "fail";                       // using ternary operator for appling conditions for pass and fail
+        studentsMarkSheets.push({ ...marks, total: totalMarks, status });          
     });
 
-    return studentsMarkSheets;
+    return studentsMarkSheets;               // return student Mark Sheet values in main functions
 }
 
 
-const studentsMarkSheets = generateStudentMarkSheets(students, Details);
-console.log(studentsMarkSheets);
+const studentsMarkSheets = generateStudentMarkSheets(students, Details);             // getting students data like name, roll no, subject marks, total marks, pass or fail condition
+console.log(studentsMarkSheets);                                     // printing out values
+ 
